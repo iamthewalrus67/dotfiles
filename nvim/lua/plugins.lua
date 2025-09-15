009fc7dbb6329915f0815e8f602c06c7d2973cdb
Bohdan <bodya.ru02@gmail.com>
2025-09-15 16:25:54 +0300

    Add git blame

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -551,6 +551,24 @@ require('lazy').setup({
         open_mapping = [[<c-\>]],
       })
     end
-  }
+  },
+
+  {
+   "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    config = function ()
+      require('gitblame').setup({
+      
+        enabled = true,  -- if you want to enable the plugin
+        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+      })
+
+      vim.keymap.set('n', '<leader>gb', ':GitBlameToggle<CR>', {noremap=true, desc='[G]it [B]lame'})
+      vim.g.gitblame_enabled = 0
+    end
+  },
 })
 
