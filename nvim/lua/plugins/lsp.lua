18cd83398509a1c6dc89b253ce9e6693578eaee7
Bohdan <bodya.ru02@gmail.com>
2025-09-26 12:45:55 +0300

    Add harper to lsp list

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -176,6 +176,40 @@ return {
         },
         rust_analyzer = {
 
+        },
+        harper_ls = {
+          filetypes = {'md', 'tex', 'bib'},
+          settings = {
+            ["harper-ls"] = {
+              userDictPath = "",
+              workspaceDictPath = "",
+              fileDictPath = "",
+              linters = {
+                SpellCheck = true,
+                SpelledNumbers = false,
+                AnA = true,
+                SentenceCapitalization = true,
+                UnclosedQuotes = true,
+                WrongQuotes = false,
+                LongSentences = true,
+                RepeatedWords = true,
+                Spaces = true,
+                Matcher = true,
+                CorrectNumberSuffix = true
+              },
+              codeActions = {
+                ForceStable = false
+              },
+              markdown = {
+                IgnoreLinkTitle = false
+              },
+              diagnosticSeverity = "hint",
+              isolateEnglish = false,
+              dialect = "American",
+              maxFileLength = 120000,
+              ignoredLintsPath = {}
+            }
+          },
         }
       } -- servers
 
