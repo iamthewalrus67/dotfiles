2e58dfd3b88c57ecc3012acdf91729f3ccf63972
Bohdan <bodya.ru02@gmail.com>
2025-09-26 11:01:43 +0300

    Add condition for loading texpresso plugin

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -2,5 +2,8 @@ return {
   {
     "let-def/texpresso.vim",
     ft = 'tex',
+    cond = function () -- shouln't be loaded if texpresso is not installed
+        return vim.fn.executable 'texpresso' == 1
+    end
   }
 }
