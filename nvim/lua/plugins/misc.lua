f1636fa63e8a3d45379660615b81c2103f0e3b65
Bohdan <bodya.ru02@gmail.com>
2025-10-01 07:10:19 +0300

    Remove git hunk

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -61,7 +61,6 @@ return {
       spec = {
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
   },
