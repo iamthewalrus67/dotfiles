fa9ed19b2c900881af136ba57365a1de56b3dfc3
Bohdan <bodya.ru02@gmail.com>
2025-10-03 10:56:02 +0300

    Add consistent toggle-term size

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -5,6 +5,8 @@ return {
     -- vim.keymap.set('n', '<C-t>', ':ToggleTerm<CR>', { desc = 'ToggleTerm' })
     require('toggleterm').setup({
       open_mapping = [[<c-\>]],
+      size = 15,
+      persist_size=false,
     })
   end
 }
