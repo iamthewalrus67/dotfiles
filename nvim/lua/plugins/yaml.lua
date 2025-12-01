1d56a3c63cf9348d4861d52be8285f1d5674e839
Bohdan <bodya.ru02@gmail.com>
2025-12-01 11:23:00 +0200

    Add yaml plugin

---
+++ nvim/lua/plugins/yaml.lua
@@ -0,0 +1,9 @@
+return {
+  "cuducos/yaml.nvim",
+  ft = { "yaml", "yml" }, -- optional
+  dependencies = {
+    "folke/snacks.nvim", -- optional
+    "nvim-telescope/telescope.nvim", -- optional
+    "ibhagwan/fzf-lua" -- optional
+  },
+}
