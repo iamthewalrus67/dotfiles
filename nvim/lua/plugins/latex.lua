9d1c483b3d855d2ef1998068c0c25f0ec396c136
Bohdan <bodya.ru02@gmail.com>
2025-12-27 11:48:36 +0200

    Add vimtex

--- nvim/lua/plugins/latex.lua
+++ nvim/lua/plugins/latex.lua
@@ -11,8 +11,22 @@ function ToggleTeXpresso()
     print("TeXpresso Started")
   end
 end
-
 return {
+    {
+    "lervag/vimtex",
+    lazy = false,     -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      -- vim.g.vimtex_view_method = "okular"
+      
+      vim.g.vimtex_view_general_viewer = 'okular'
+      vim.g.vimtex_view_general_options = '--unique file:@pdf\\#src:@line@tex'
+      vim.g.vimtex_compiler_method = "tectonic"
+      vim.g.vimtex_compiler_enabled = 1
+
+    end
+    },
   {
     "let-def/texpresso.vim",
     ft = 'tex',
--- nvim/lua/plugins/syntax_highlight.lua
+++ nvim/lua/plugins/syntax_highlight.lua
@@ -11,6 +11,7 @@ return {
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex', },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
